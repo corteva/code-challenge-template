cecf2e83b53845e35c09556b6b4a56ba8fa12a27
Tommy O'Brien <tommy.obrien502@gmail.com>
2022-09-03 14:41:20 -0500

    crop filter on year works

--- src/pipeline/crop/views.py
+++ src/pipeline/crop/views.py
@@ -1,5 +1,7 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import ListAPIView
 
+from crop.filters import CropFilter
 from crop.models import CropData
 from crop.serializers import CropSerializer
 
@@ -7,3 +9,5 @@ from crop.serializers import CropSerializer
 class CropListView(ListAPIView):
     queryset = CropData.objects.all()
     serializer_class = CropSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = CropFilter
