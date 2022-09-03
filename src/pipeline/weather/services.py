5d51c1dd5d4ab9dac84adf8a46c68369de68279d
Tommy O'Brien <tommy.obrien502@gmail.com>
2022-09-03 14:58:12 -0500

    calculate statistics methods

--- src/pipeline/weather/services.py
+++ src/pipeline/weather/services.py
@@ -1,6 +1,6 @@
-from django.db.models import Max, Min
+from django.db.models import Max, Min, Avg, Sum
 
-from weather.models import WeatherData
+from weather.models import WeatherData, Statistics
 
 
 def generate_years_list():
@@ -24,4 +24,42 @@ def calculate_stats(years: list):
     station_ids = WeatherData.objects.values_list("station_id").distinct()
     for station_id in station_ids:
         for year in years:
-            pass
+            avg_max_temp = _calculate_avg_max_temp(usable_max_data, station_id, year)
+            avg_min_temp = _calculate_avg_min_temp(usable_min_data, station_id, year)
+            total_precipitation = _calculate_total_precip(
+                usable_precip_data, station_id, year
+            )
+            Statistics.objects.create(
+                station_id=station_id,
+                year=year,
+                avg_max_temp=avg_max_temp,
+                avg_min_temp=avg_min_temp,
+                total_precipitation=total_precipitation,
+            )
+
+
+def _calculate_avg_max_temp(usable_max_data, station_id, year):
+    try:
+        filtered_data = usable_max_data.filter(station_id=station_id, year=year)
+        avg_max_temp = filtered_data.aggregate(Avg("max_temp"))
+        return avg_max_temp
+    except Exception:
+        return None
+
+
+def _calculate_avg_min_temp(usable_min_data, station_id, year):
+    try:
+        filtered_data = usable_min_data.filter(station_id=station_id, year=year)
+        avg_min_temp = filtered_data.aggregate(Avg("min_temp"))
+        return avg_min_temp
+    except Exception:
+        return None
+
+
+def _calculate_total_precip(usable_precip_data, station_id, year):
+    try:
+        filtered_data = usable_precip_data.filter(station_id=station_id, year=year)
+        total_precipitation = filtered_data.aggregate(Sum("precipitation"))
+        return total_precipitation
+    except Exception:
+        return None
