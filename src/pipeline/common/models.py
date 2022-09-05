abe5008c47daf552eb6f0843499e07aa218cf744
Tommy O'Brien <tommy.obrien502@gmail.com>
2022-09-05 14:39:08 -0500

    add timestamp fields to base model

--- src/pipeline/common/models.py
+++ src/pipeline/common/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class BaseModel(models.Model):
+    created_timestamp = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_timestamp = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     objects = models.Manager()
 
